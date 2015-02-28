9f9a7438ed1864f74e77a8249822e6a7a574d956
Daniil Lopatin <solid.danil@gmail.com>
2015-02-28 22:20:55 +0300

    Codeforces. Round 294. A-D

---
+++ Codeforces/src/com/dlopatin/codeforce/r294/A.java
@@ -0,0 +1,60 @@
+package com.dlopatin.codeforce.r294;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * http://codeforces.com/contest/519/problem/A the queen's weight is 9, the rook's weight is 5, the bishop's weight is
+ * 3, the knight's weight is 3, the pawn's weight is 1,
+ */
+public class A {
+
+	public static void main(String[] args) {
+		new A().doJob();
+	}
+
+	private void doJob() {
+		try (Scanner scanner = new Scanner(System.in)) {
+			Map<Character, Integer> map = new HashMap<>();
+			map.put('Q', 9);
+			map.put('R', 5);
+			map.put('B', 3);
+			map.put('N', 3);
+			map.put('P', 1);
+			map.put('K', 0);
+			map.put('q', 9);
+			map.put('r', 5);
+			map.put('b', 3);
+			map.put('n', 3);
+			map.put('p', 1);
+			map.put('k', 0);
+
+			int white = 0;
+			int black = 0;
+			for (int i = 0; i < 8; i++) {
+				char[] chars = scanner.next().toCharArray();
+				for (int j = 0; j < 8; j++) {
+					char ch = chars[j];
+					if ('.' != ch) {
+						if (Character.isLowerCase(ch)) {
+							black += map.get(ch);
+						} else {
+							white += map.get(ch);
+						}
+					}
+				}
+			}
+			String res = "";
+			if (white == black) {
+				res = "Draw";
+			} else if (white > black) {
+				res = "White";
+			} else {
+				res = "Black";
+			}
+			System.out.println(res);
+		}
+	}
+
+}
---
+++ Codeforces/src/com/dlopatin/codeforce/r294/B.java
@@ -0,0 +1,60 @@
+package com.dlopatin.codeforce.r294;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * http://codeforces.com/contest/519/problem/B
+ */
+public class B {
+
+	public static void main(String[] args) {
+		new B().doJob();
+	}
+
+	private void doJob() {
+		try (Scanner scanner = new Scanner(System.in)) {
+			int n = scanner.nextInt();
+			List<Integer> init = new ArrayList<>();
+			List<Integer> first = new ArrayList<>();
+			List<Integer> second = new ArrayList<>();
+			for (int i = 0; i < n; i++) {
+				int nextInt = scanner.nextInt();
+				init.add(nextInt);
+			}
+			for (int i = 0; i < n - 1; i++) {
+				int nextInt = scanner.nextInt();
+				first.add(nextInt);
+			}
+			for (int i = 0; i < n - 2; i++) {
+				int nextInt = scanner.nextInt();
+				second.add(nextInt);
+			}
+
+			Collections.sort(init);
+			Collections.sort(first);
+			Collections.sort(second);
+
+			int res = init.get(init.size() - 1);
+			for (int i = 0; i < first.size(); i++) {
+				if (!first.get(i).equals(init.get(i))) {
+					res = init.get(i);
+					break;
+				}
+			}
+			System.out.println(res);
+
+			res = first.get(first.size() - 1);
+			for (int i = 0; i < second.size(); i++) {
+				if (!second.get(i).equals(first.get(i))) {
+					res = first.get(i);
+					break;
+				}
+			}
+			System.out.println(res);
+		}
+	}
+
+}
