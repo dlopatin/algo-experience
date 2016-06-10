486f7436397fab1879e9b8e098f9a28079da1412
Daniil Lopatin <solid.danil@gmail.com>
2016-06-10 20:58:44 +0300

    Uva

--- UVa/src/com/dlopatin/uva/datastructures/collection/p10172/Main.java
+++ UVa/src/com/dlopatin/uva/datastructures/collection/p10172/Main.java
@@ -1,8 +1,13 @@
 package com.dlopatin.uva.datastructures.collection.p10172;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
 
 /*
  * https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=629&page=show_problem&problem=1113
@@ -14,31 +19,57 @@ public class Main {
 	}
 
 	private void doJob() throws IOException {
-		try (BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in))) {
+		try (Scanner scanner = new Scanner(System.in)) {
 			StringBuilder result = new StringBuilder();
-			String line = "";
-			int caseIdx = 0;
-			while (!(line = buffer.readLine()).equals("end")) {
-				int[] stacks = new int[26];
-				char[] input = line.toCharArray();
-				for (char ch : input) {
-					ch -= 'A';
-					int idx = ch;
-					boolean found = false;
-					while (!found && idx < stacks.length) {
-						if (stacks[idx] > 0) {
-							found = true;
-							stacks[idx] = 0;
-						}
-						idx++;
+			int n = scanner.nextInt();
+			while (n-- > 0) {
+				int N = scanner.nextInt();
+				int S = scanner.nextInt();
+				int Q = scanner.nextInt();
+				List<Queue<Integer>> stations = new ArrayList<>();
+				for (int i = 0; i < N; i++) {
+					int cargosInQueue = scanner.nextInt();
+					Queue<Integer> station = new LinkedList<>();
+					for (int j = 0; j < cargosInQueue; j++) {
+						station.add(scanner.nextInt() - 1);
 					}
-					stacks[ch] = 1;
+					stations.add(station);
 				}
-				int count = 0;
-				for (int stackUsed : stacks) {
-					count += stackUsed;
+
+				Deque<Integer> carrier = new ArrayDeque<>(S);
+				int emptyStations = 0;
+				int index = 0;
+				int time = -2;
+				while (!carrier.isEmpty() || emptyStations < N) {
+					time += 2; // moving time
+					index %= N;
+					Queue<Integer> station = stations.get(index);
+					// unload
+					while (carrier.size() > 0 && Q != station.size()) {
+						time++;
+						Integer cargo = carrier.pop();
+						if (cargo != index) {
+							station.add(cargo);
+						}
+					}
+					while (carrier.size() > 0 && carrier.peekFirst() == index) {
+						carrier.pop();
+						time++;
+					}
+
+					// load
+					while (carrier.size() != S && station.size() > 0) {
+						carrier.addFirst(station.remove());
+						time++;
+					}
+
+					int[] counter = new int[1];
+					stations.forEach(s -> counter[0] += s.isEmpty() ? 1 : 0);
+					emptyStations = counter[0];
+
+					index++;
 				}
-				result.append(String.format("Case %d: %d\n", ++caseIdx, count));
+				result.append(time >= 0 ? time : 0).append("\n");
 			}
 			System.out.print(result.toString());
 		}
