27a9989a52bdd18ed919fd1a67661b8085427986
Daniil Lopatin <solid.danil@gmail.com>
2018-10-26 00:11:11 +0300

    Uva 11572

---
+++ UVa/src/com/dlopatin/uva/datastructures/nonlineards/p11572/Main.java
@@ -0,0 +1,42 @@
+package com.dlopatin.uva.datastructures.nonlineards.p11572;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=27&page=show_problem&problem=2619
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Main.doJob();
+    }
+
+    private static void doJob() throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            int t = Integer.parseInt(reader.readLine());
+            Map<Integer, Integer> map = new HashMap<>();
+            while (t-- > 0) {
+                map.clear();
+                int n = Integer.parseInt(reader.readLine());
+                int max = 0;
+                int idx = 0;
+                int lastOccur = 0;
+                while (n-- > 0) {
+                    int id = Integer.parseInt(reader.readLine());
+                    idx++;
+                    if (map.containsKey(id)) {
+                        int pos = map.get(id);
+                        lastOccur = Math.max(pos, lastOccur);
+                    }
+                    max = Math.max(max, idx - lastOccur);
+                    map.put(id, idx);
+                }
+                System.out.println(max);
+            }
+        }
+    }
+}
