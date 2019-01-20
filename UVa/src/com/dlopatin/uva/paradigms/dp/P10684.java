9b5a0b9392391ddb2f0ed0bc092009589f0f9364
Daniil Lopatin <solid.danil@gmail.com>
2019-01-20 23:45:38 +0300

    Uva DP: 787 (bottom up solution), 10648 (Kadane’s algorithm)

---
+++ UVa/src/com/dlopatin/uva/paradigms/dp/P10684.java
@@ -0,0 +1,46 @@
+package com.dlopatin.uva.paradigms.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+import static java.lang.Integer.parseInt;
+
+/*
+ * https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=1625
+ * The jackpot
+ */
+public class P10684 {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            StringBuilder result = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null && !"0".equals(line.trim())) {
+                line = line.trim();
+                if (line.isEmpty()) {
+                    continue;
+                }
+                int n = parseInt(line);
+                int localSum = 0;
+                int globalSum = Integer.MIN_VALUE;
+                StringTokenizer dataTokens = new StringTokenizer(reader.readLine());
+                for (int i = 0; i < n; i++) {
+                    if (!dataTokens.hasMoreTokens()) {
+                        dataTokens = new StringTokenizer(reader.readLine());
+                    }
+                    int val = parseInt(dataTokens.nextToken());
+                    localSum = Math.max(val, localSum + val);
+                    globalSum = Math.max(globalSum, localSum);
+                }
+                result.append(globalSum > 0
+                        ? String.format("The maximum winning streak is %d.", globalSum)
+                        : "Losing streak."
+                ).append("\n");
+            }
+            System.out.print(result);
+        }
+    }
+}
+
---
+++ UVa/src/com/dlopatin/uva/paradigms/dp/P787.java
@@ -0,0 +1,63 @@
+package com.dlopatin.uva.paradigms.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+import static java.lang.Integer.parseInt;
+
+/*
+ * https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=728
+ * Maximum Sub-sequence Product
+ */
+public class P787 {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            StringBuilder result = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null && !line.isEmpty()) {
+                StringTokenizer dataTokens = new StringTokenizer(line);
+                int n = 0;
+                BigInteger[][] cache = new BigInteger[105][105];
+                BigInteger[] data = new BigInteger[103];
+                BigInteger prod = BigInteger.valueOf(-999999);
+                while (dataTokens.hasMoreTokens()) {
+                    int next = parseInt(dataTokens.nextToken());
+                    if (next == -999999) {
+                        break;
+                    }
+                    BigInteger nextBig = BigInteger.valueOf(next);
+                    data[n] = nextBig;
+                    cache[0][n] = nextBig;
+                    prod = prod.max(nextBig);
+                    n++;
+                }
+//                for (int i = 0; i < n; i++) {
+//                    BigInteger curr = BigInteger.valueOf(1);
+//                    for (int j = i; j < n; j++) {
+//                        curr = curr.multiply(data[j]);
+//                        if (curr.compareTo(prod) > 0) {
+//                            prod = curr;
+//                        }
+//                    }
+//                }
+
+                for (int len = 1; len < n; len++) {
+                    for (int pos = 0; pos < n - len; pos++) {
+                        BigInteger multiply = cache[len - 1][pos].multiply(data[pos + len]);
+                        cache[len][pos] = multiply;
+                        prod = prod.max(multiply);
+                    }
+                }
+                result.append(prod).append("\n");
+            }
+            System.out.print(result);
+        }
+
+    }
+
+}
+
