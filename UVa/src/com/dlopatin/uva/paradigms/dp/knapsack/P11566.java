1b5787035070ba942e14c9d40628245db38c97bc
Daniil Lopatin <solid.danil@gmail.com>
2019-02-05 01:28:48 +0300

    Uva DP Knapsack:  - 11566

--- UVa/src/com/dlopatin/uva/paradigms/dp/knapsack/P11566.java
+++ UVa/src/com/dlopatin/uva/paradigms/dp/knapsack/P11566.java
@@ -3,6 +3,7 @@ package com.dlopatin.uva.paradigms.dp.knapsack;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -14,54 +15,72 @@ import static java.lang.Integer.parseInt;
  */
 public class P11566 {
 
-    private static int[][] dp;
+    private static int[][][] dp;
 
     public static void main(String[] args) throws NumberFormatException, IOException {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             StringBuilder result = new StringBuilder();
             String line;
-            while ((line = reader.readLine()) != null && !"0 0 0 0".equals(line)) {
+            while (!"0 0 0 0".equals(line = readNotNullLine(reader))) {
                 StringTokenizer nxtkTokens = new StringTokenizer(line);
                 int n = parseInt(nxtkTokens.nextToken());
                 int x = parseInt(nxtkTokens.nextToken());
                 int t = parseInt(nxtkTokens.nextToken());
-                int k = parseInt(nxtkTokens.nextToken()) + 1;
-                int[] values = new int[k];
-                int[] weights = new int[k];
-                for (int i = 0; i < values.length; i++) {
+                int k = parseInt(nxtkTokens.nextToken());
+                int[] values = new int[k * 2];
+                int[] weights = new int[k * 2];
+                for (int i = 0; i < k; i++) {
                     StringTokenizer dataTokens = new StringTokenizer(reader.readLine());
-                    weights[i] = parseInt(dataTokens.nextToken());
-                    values[i] = parseInt(dataTokens.nextToken());
+                    int price = parseInt(dataTokens.nextToken());
+                    weights[i * 2] = price;
+                    weights[i * 2 + 1] = price;
+                    int value = 0;
+                    for (int j = 0; j <= n; j++) {
+                        value += parseInt(dataTokens.nextToken());
+                    }
+                    values[i * 2] = value;
+                    values[i * 2 + 1] = value;
                 }
-                dp = new int[n][201];
-                for (int[] ints : dp) {
-                    Arrays.fill(ints, -1);
+                int div = BigDecimal.valueOf(x * (n + 1))
+                        .divide(new BigDecimal("1.1"), BigDecimal.ROUND_DOWN).intValue();
+                int cost = div - t * (n + 1);
+                dp = new int[2 * k][cost + 1][2 * (n + 1) + 1];
+                for (int[][] ints2d : dp) {
+                    for (int[] ints : ints2d) {
+                        Arrays.fill(ints, -1);
+                    }
                 }
-                int cnt = knapsack(n - 1, 0, 10000, values, weights);
-                result.append(cnt).append("\n");
+                int cnt = knapsack(2 * k - 1, cost, 2 * (n + 1), values, weights);
+                result.append(String.format("%.2f\n", (double) cnt / (n + 1)));
             }
             System.out.print(result);
         }
     }
 
-    private static int knapsack(int idx, int spent, int max, int[] values, int[] weights) {
-        if (spent > max && spent < 1800 || spent > max + 200) {
-            return Integer.MIN_VALUE;
-        }
-        if (idx < 0) {
-            if (spent > max && spent <= 2000) {
-                return Integer.MIN_VALUE;
-            }
+    private static int knapsack(int idx, int leftMoney, int leftDishes, int[] values, int[] weights) {
+        if (idx < 0 || leftMoney == 0 || leftDishes == 0) {
             return 0;
         }
-        if (dp[idx][spent] != -1) {
-            return dp[idx][spent];
+        if (dp[idx][leftMoney][leftDishes] != -1) {
+            return dp[idx][leftMoney][leftDishes];
         }
-
-        int result = Math.max(knapsack(idx - 1, spent, max, values, weights),
-                values[idx] + knapsack(idx - 1, spent + weights[idx], max, values, weights));
-        dp[idx][spent] = result;
+        int result = 0;
+        if (weights[idx] > leftMoney) {
+            result = knapsack(idx - 1, leftMoney, leftDishes, values, weights);
+        } else {
+            result = Math.max(knapsack(idx - 1, leftMoney, leftDishes, values, weights),
+                    values[idx] + knapsack(idx - 1, leftMoney - weights[idx], leftDishes - 1, values, weights));
+        }
+        dp[idx][leftMoney][leftDishes] = result;
         return result;
+    }
+
+    private static String readNotNullLine(BufferedReader reader) throws IOException {
+        String line = reader.readLine();
+        while (line == null || line.isEmpty()) {
+            line = reader.readLine();
+        }
+        return line;
     }
 
 }
